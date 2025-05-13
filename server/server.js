4aa9cb2693287f438ce621bbeabf1c0e53d2fec2
Vivekbhand09 <bhandvivek974@gmail.com>
2025-05-13 19:36:42 +0530

    updated server.js and App.js

--- server/server.js
+++ server/server.js
@@ -52,6 +52,10 @@ app.use("/api/messages",messageRouter)
 // Connect to MongoDb 
 await connectDB();
 
-
-const PORT =process.env.PORT || 5000;
+if(process.env.NODE_ENV !=="production"){
+    const PORT =process.env.PORT || 5000;
 server.listen(PORT,()=>console.log("Server is running on PORT: "+PORT));
+}
+
+// Export Server for Versel
+export default server;
